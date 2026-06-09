4d1ce086d8c416dad6a4a8479e802cf83b0fc2cb
yourname <youremail>
2026-06-10 07:47:56 +0800

    Revert "fix: wrapper width:100%代替flex:1 消除双重聚焦边框"
    This reverts commit 38dc0cffc7112175f487ec8afe6176d77e87b0c5.

--- frontend/src/views/AdminAuditView.vue
+++ frontend/src/views/AdminAuditView.vue
@@ -382,16 +382,17 @@ onMounted(load)
 /* 搜索栏 */
 .toolbar > .el-input {
   width: 340px;
+  display: flex;
 }
 .toolbar > .el-input :deep(.el-input__wrapper) {
-  width: 100% !important;
+  flex: 1;
   box-sizing: border-box !important;
 }
 .toolbar > .el-input :deep(.el-input__wrapper:hover) {
   box-shadow: 0 0 0 1px var(--border-color) inset !important;
 }
 .toolbar > .el-input :deep(.el-input__wrapper.is-focus) {
-  box-shadow: 0 0 0 1px #f97316 inset !important;
+  box-shadow: 0 0 0 1px #f97316 !important;
 }
 
 /* 筛选栏 */
